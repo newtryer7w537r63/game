77351c1ababc460ac5869a05cf2e4783160a87f1
newtryer7w537r63 <sigmarizzlerskibdiaiden@gmail.com>
2026-04-15 16:23:48 -0700

    Create graphics.h

---
+++ graphics.h
@@ -0,0 +1,10 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+void clearScreen();
+void drawPlayer(int x, int y);
+void drawBlock(int x, int y);
+void drawScore(int score);
+void drawText(int x, int y, char *text);
+
+#endif
